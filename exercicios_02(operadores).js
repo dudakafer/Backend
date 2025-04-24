0c5d26ad2efa8f30260dd766603101c774771e38
dudakafervitoria <eduardakafervitoria@gmail.com>
2025-04-24 10:11:11 -0400

    atividades operadores

---
+++ exercicios_02(operadores).js
@@ -0,0 +1,27 @@
+//01
+ 
+let n1 = 30;
+let n2 = 20;
+console.log(" 30 + 20 é",n1 + n2);
+console.log(" 30 - 20 é",n1- n2);
+console.log(" 30 * 20 é",n1 * n2);
+console.log(" 30 / 20 é",n1 / n2);
+
+// 02 
+
+let pessoa1 = 20;
+let pesssoa2 = 40;
+let comparar = pessoa1 > pesssoa2 ? 'pessoa1 é mais velha' : 'pesssoa 2 é mais velha';
+console.log (comparar);
+
+//03
+
+let idade = 22;
+let comparar2 = idade >= 22 ? "Maior de idade" : "Menor de idade";
+console.log (comparar2);
+
+//04
+
+let variavel = 8;
+let verificar = variavel % 2 == 0 ? " 8 é par" : " 8 e impar";
+console.log (verificar);
